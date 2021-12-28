172c93583b106ce8030d39d4a3dce816dca114cc
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-12-28 11:10:55 +0100

    correction 2 : consommation d'un service dans un composant

--- ams/src/app/services/student.service.ts
+++ ams/src/app/services/student.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
-
+import { Etudiant } from '../entities';
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
-
+  tab:Etudiant[]=[
+    {"id":2,"nom":"Ahmed","poste":"Prof"},
+    {"id":3,"nom":"Ali","poste":"Chef"},
+    {"id":5,"nom":"Mahdi","poste":"Ing"}
+  ];
   constructor() { }
 
   getInfo()
   {
     return "Vous êtes un excellent etudiant";
+  }
+  getStudentList()
+  {
+    return this.tab;
   }
 }
